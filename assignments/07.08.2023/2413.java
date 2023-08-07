bcd4fc6a2bca322f007bfc180990a6102dea1c6a
Death11068 <haardpatel11068@gmail.com>
2023-08-07 14:54:31 +0530

    lab1 submmisiom

---
+++ assignments/07.08.2023/2413.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2==1){
+            int mul=n*2;
+            return mul;
+        }
+        else{
+            int mul=n;
+            return mul;
+        }
+        
+    }
+}
